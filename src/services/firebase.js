2697a41d42896b6988e98f233e4b47f54fa01a55
Arisa <arisa.kareeda@example.com>
2026-09-15 01:35:21 +0700

    fix: correct Firebase web app configuration

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDN-IGLe9ykq0kDJd8X9UqwPC2qS8ksam0",
+  apiKey: "AIzaSyDN-IGlE9ykqOkDJd8X9UqwPC2qs8ksam0",
   authDomain: "exercise-7daec.firebaseapp.com",
   projectId: "exercise-7daec",
-  storageBucket: "exercise-7daec.appspot.com",
+  storageBucket: "exercise-7daec.firebasestorage.app",
   messagingSenderId: "848108890571",
-  appId: "1:848108890571:web:e331b04cfb053888ae4e"
+  appId: "1:848108890571:web:e331b04cfb0538888aae4e"
 };
 
 const app = initializeApp(firebaseConfig);
