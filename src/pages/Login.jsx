6a98a606328362ed4a7674ec73e46b2a0f01e3c7
Arisa <arisa.kareeda@example.com>
2026-09-15 11:27:29 +0700

    up

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
 import { signInWithEmailAndPassword } from 'firebase/auth';
-import { auth } from '../services/firebase';
+import { auth, db } from '../firebase';
 
 export default function Login() {
   const [email, setEmail] = useState('');
